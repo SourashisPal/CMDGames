7b8f1bf4b20d57cb08c79c45b6a92da5c2ea331a
Sourashis Pal <sourashis.pal2008@gmail.com>
2021-10-10 21:57:45 +0530

    (#3)

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"in.sourashis.tictactoe","l":"Main"},{"p":"in.sourashis.tictactoe","l":"TicTacToe"}];updateSearchResults();
